24bc0599d7e816c5f485ba5fd4377aa53817aebf
SamuelFiais <samuelfiais20@gmail.com>
2020-12-23 14:29:05 -0300

    Update, Remove e Raname Files

---
+++ js/tic-tac-toe.js
@@ -0,0 +1,13 @@
+const tic_tac_toe = {
+    board: ['', '', '', '', '', '', '', '', '',],
+    simbols: ['X', 'Y'],
+    container_elements: null,
+
+    init(container) {
+        this.container_elements = container;
+    },
+
+    draw() {
+
+    },
+}
